e1a16c8d8afac10ae1961510ee08cae8ebf73b7e
Salad <szolek1337@gmail.com>
2025-10-04 15:04:58 +0200

    Initial commit

---
+++ src/main/java/com/medicaloffice/medicalofficemanager/MedicalOfficeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.medicaloffice.medicalofficemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOfficeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalOfficeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medicaloffice/medicalofficemanager/MedicalOfficeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicaloffice.medicalofficemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOfficeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
